c12ed50950c95c219f83ead22ba7b4aebdef831d
Viduminiiii <vidumini.dayananda@gmail.com>
2024-06-18 03:04:13 +0530

    Implementing contact page

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './contact.css'
+
+function Contact(){
+    return(
+        <div className="App">
+            <div className="container">
+                <div className="dedtails">
+                    <h1>Helloooo</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
